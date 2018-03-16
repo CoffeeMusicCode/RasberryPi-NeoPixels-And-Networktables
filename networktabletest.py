6e161fafc47eadb3c2d3dc97e426b1e640cbfc3d
Jon Kiddy <jon.kiddy@gmail.com>
2018-03-16 14:23:01 -0400

    set the states to each of the network table variables

--- networktabletest.py
+++ networktabletest.py
@@ -8,9 +8,15 @@ logging.basicConfig(level=logging.DEBUG)
 NetworkTables.initialize()
 nt = NetworkTables.getTable('RasberryPi')
 
+state = 'Disabled'
+toggle = 1
+detaunt1 = 10
+detaunt2 = 5
+
 while True:
-  nt.putString('state', 'Disabled') # Disabled Auto TeleOp
-  nt.putNumber('toggle', 1) # 0 1
-  nt.putNumber('detaunt1', 1) # 0 - 10
-  nt.putNumber('detaunt2', 5) # 0 - 10
+  nt.putString('state', state) # Disabled Auto TeleOp
+  nt.putNumber('toggle', toggle) # 0 1
+  nt.putNumber('detaunt1', detaunt1) # 0 - 10
+  nt.putNumber('detaunt2', detaunt2) # 0 - 10
+  print(state, toggle, detaunt1, detaunt2)
   time.sleep(1)
